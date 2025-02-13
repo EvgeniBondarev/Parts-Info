deed7826ab9aee7a120251faebd3f93fe08172ae
vasiliy_k <vasiliy_k@holdingbp.ru>
2025-02-13 10:00:38 +0300

    mini fix

--- src/components/SmallCard.jsx
+++ src/components/SmallCard.jsx
@@ -5,13 +5,13 @@ const SmallCard = ({
   color = "from-blue-400 to-blue-600",
   className = "",
   textCenter = false,
-  padding = 'p-6',
+  padding = 'p-4',
   onClick
 }) => {
   return (
     <div
       onClick={onClick}
-      className={`group relative ${padding} rounded-xl shadow-lg hover:shadow-xl 
+      className={`group relative ${padding} rounded-xl shadow-lg hover:shadow-xl cursor-pointer
         bg-gradient-to-r ${color} 
         transition-all duration-300 
         transform hover:-translate-y-1.5 
@@ -22,7 +22,7 @@ const SmallCard = ({
       <div className="absolute bottom-0 left-0 w-full h-1 bg-white/20" />
       
       <div className="relative">
-        <h2 className={`text-xl ${textCenter ? 'text-center' : ''} font-bold text-white drop-shadow-md`}>
+        <h2 className={`text-xl ${textCenter ? 'text-center' : ''}  font-bold text-white drop-shadow-md`}>
           {title}
         </h2>
         
